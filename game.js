3486fd4f3da366c7200c1505c76174b6d529e4a2
axel0313 <axel.svendsen@hotmail.com>
2022-08-18 19:02:46 +0200

    fixa så att man kan ha random ordning på personer

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 var pointer = 0;
 var score = 0;
 
+// ändra denna för att sätta på eller stänga av random ordning 1 = random    0 = inte random
+var random = 1
+
+
 const images = ["personer/donald_trump.png",
   "personer/ryan_gosling.png",
   "personer/20220715_213252.jpg",
@@ -17,6 +21,8 @@ const people = ["donald_trump<donald<trump",
   "elon_musk<elon<musk",
   "Felix_Kjellberg<felix<pewdi"];
 
+const random_array = shuffle(generate_array(people.length))
+
 
 // definerar pointers till element i dokumentet (game.html)
 const inp = document.getElementById("inp");
@@ -25,6 +31,9 @@ const display_results_text = document.getElementById("results");
 const next_button = document.getElementById("next_person");
 const guess_button = document.getElementById("guess");
 const score_disp = document.getElementById("score");
+
+// ser till att första bilden altid är rätt person
+pic.src = images[getPointerVal(pointer)]
 
 // har hand om att förmedla ifall svaret var rätt eller fel till användaren
 function results(correct) {
@@ -52,10 +61,12 @@ function results(correct) {
 
 // vad som händer när man klickar på test/gissa knaååen
 function clicked() {
+
+
   val = inp.value;
   val = val.toLowerCase();
   val = val.replace(" ", "_")
-  const alla_svar = people[pointer].split("<");
+  const alla_svar = people[getPointerVal(pointer)].split("<");
 
   if (alla_svar.includes(val)) {
     results(1)
@@ -77,9 +88,47 @@ function clicked() {
 function next_person() {
   pointer = pointer + 1
   console.log(pointer)
-  pic.src = images[pointer];
+  pic.src = images[getPointerVal(pointer)];
   display_results_text.style.visibility = "hidden";
   next_button.style.visibility = "hidden";
   guess_button.style.visibility = "visible";
 
+}
+
+function generate_array(length){
+  var arr = [];
+  x = 0;
+  while (x != length){
+    arr.push(x);
+    x += 1;
+  }
+  return arr;
+
+}
+
+function getPointerVal(pointer){
+  if (random == 1){
+    return random_array[pointer];
+  }
+  else{
+    return pointer
+  }
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
